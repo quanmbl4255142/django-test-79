ddcaee86d948326562c626a53dbfb2b7c7c2271f
quanmbl4255142 <nguyenminhquan4255142@gmail.com>
2026-01-06 16:02:26 +0700

    Add comment in UserSerializer class
    Added a comment in the UserSerializer class.

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    # aaaaa
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'created_at', 'created_at', 'updated_at']
